10f3877c27922bc5764ae8ed02a2ffad2f70f0ee
md shoharab pk <76746357+web-shoharab-pk@users.noreply.github.com>
2022-02-15 22:45:38 +0600

    filter system added

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -1,4 +1,8 @@
+// import "bootstrap/less/bootstrap.less"
+import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
 import React, { Fragment, useEffect, useState } from 'react';
+import Pagination from "react-js-pagination";
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
 import { getProduct } from '../../actions/productAction';
@@ -7,10 +11,6 @@ import LoaderTwo from '../Loader/LoaderTwo';
 import './../Home/Home.css';
 import Header from './../layout/Header/header';
 import './Products.css';
-import Pagination from "react-js-pagination";
-// import "bootstrap/less/bootstrap.less"
-import Slider from '@mui/material/Slider';
-import Typography from '@mui/material/Typography';
 
 const categories = [
     "Laptop",
@@ -26,8 +26,8 @@ const Products = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [price, setPrice] = useState([0, 25000]);
     const [category, setCategory] = useState("")
-
-
+    const [ratings, setRatings] = useState(0)
+console.log("ratings", ratings)
     const dispatch = useDispatch()
     const { products, loading, error, productsCount, resultPerPage, } = useSelector(state => state.products)
     const { keyword } = useParams();
@@ -36,15 +36,15 @@ const Products = () => {
         setCurrentPage(e)
     }
 
-    const priceHandler = (e, newPrice) => {  
-            setPrice(newPrice) 
+    const priceHandler = (e, newPrice) => {
+        setPrice(newPrice)
     }
 
     useEffect(() => {
         setTimeout(() => {
-        dispatch(getProduct(keyword, currentPage, price, category))
+            dispatch(getProduct(keyword, currentPage, price, category, ratings))
         }, 500);
-    }, [dispatch, keyword, currentPage, price, category]);
+    }, [dispatch, keyword, currentPage, price, category, ratings]);
     return (
         <Fragment>
             <Header /><br /><br />
@@ -65,22 +65,22 @@ const Products = () => {
                             <Typography>Price</Typography>
                             <br />
                             <Slider
-                            value={price}
-                            onChange={priceHandler}
-                            valueLabelDisplay="auto"
-                            aria-labelledby='range-slider'
-                            min={0}
-                            max={25000}
-                            /> 
+                                value={price}
+                                onChange={priceHandler}
+                                valueLabelDisplay="auto"
+                                aria-labelledby='range-slider'
+                                min={0}
+                                max={25000}
+                            />
 
                             <Typography>Categories</Typography>
                             <ul className="categoryBox">
                                 {
                                     categories.map((category) => (
                                         <li
-                                        className="category-link"
-                                    key={category}
-                                    onClick={() => setCategory(category)}
+                                            className="category-link"
+                                            key={category}
+                                            onClick={() => setCategory(category)}
                                         >
                                             {category}
                                         </li>
@@ -88,26 +88,40 @@ const Products = () => {
                                 }
                             </ul>
 
+                            <fieldset>
+                                <legend component="legend">Rating Above</legend>
+                                <Slider
+                                    value={ratings}
+                                    onChange={(e, newRating) => {
+                                        setRatings(newRating);
+                                    }}
+                                    aria-labelledby="continuous-slider"
+                                    valueLabelDisplay="auto"
+                                    min={0}
+                                    max={5}
+                                />
+                            </fieldset>
+
                         </div>
                         {
-                           (( resultPerPage < productsCount ) && (products.length !== 0)) && 
+                            ((resultPerPage < productsCount) && (products.length !== 0)) &&
                             (
                                 <div className="paginationBox">
-                                <Pagination
-                                activePage={currentPage}
-                                itemsCountPerPage={resultPerPage}
-                                totalItemsCount={productsCount}
-                                onChange={setCurrentPageNo}
-                                nextPageText="Next"
-                                prevPageText="Prev"
-                                firstPageText="1st"
-                                lastPageText="Last"
-                                itemClass="page-item"
-                                linkClass="page-link"
-                                activeClass='pageItemActive'
-                                activeLinkClass='pageLinkActive'
-                                />
-                            </div>
+                                    <Pagination
+                                        activePage={currentPage}
+                                        itemsCountPerPage={resultPerPage}
+                                        totalItemsCount={productsCount}
+                                        onChange={setCurrentPageNo}
+                                        nextPageText="Next"
+                                        prevPageText="Prev"
+                                        firstPageText="1st"
+                                        lastPageText="Last"
+                                        itemClass="page-item"
+                                        linkClass="page-link"
+                                        activeClass='pageItemActive'
+                                        activeLinkClass='pageLinkActive'
+                                    />
+                                </div>
                             )
                         }
                     </Fragment>

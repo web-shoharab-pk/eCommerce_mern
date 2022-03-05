7d48255cf738d3e85df1465e0a2bab157cc9ed06
md shoharab pk <76746357+web-shoharab-pk@users.noreply.github.com>
2022-03-05 22:43:28 +0600

    login frontend done

--- frontend/src/component/layout/Header/header.js
+++ frontend/src/component/layout/Header/header.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
-import { Container, Nav, Navbar, NavLink } from 'react-bootstrap';
-import { Link, NavLink as ActiveRoute } from 'react-router-dom';
+import { Nav, Navbar, NavLink } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import "./Header.css";
 
 const header = () => {
@@ -11,24 +11,24 @@ const header = () => {
     <Fragment>
       <Navbar className="header-navbar" fixed="top" variant="dark" expand="lg">
         {/* <Container> */}
-          <Navbar.Brand href="/">
-            ECOMMERCE
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-               <Link component={NavLink} to="/"> Home</Link> 
-               <Link component={NavLink} to="/account">Account</Link> 
-              <Link component={NavLink} to="/products">Products</Link>
-              <Link component={NavLink} to="/contact">Contact</Link>
-              <Link component={NavLink} to="/search">Search</Link>
-              <Link component={NavLink} to="/about">About</Link>
-              <Link component={NavLink} to="/login">Login</Link>
-            </Nav>
-          </Navbar.Collapse>
+        <Navbar.Brand href="/">
+          ECOMMERCE
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link component={NavLink} to="/"> Home</Link>
+            <Link component={NavLink} to="/account">Account</Link>
+            <Link component={NavLink} to="/products">Products</Link>
+            <Link component={NavLink} to="/contact">Contact</Link>
+            <Link component={NavLink} to="/search">Search</Link>
+            <Link component={NavLink} to="/about">About</Link>
+            <Link component={NavLink} to="/login">Login</Link>
+          </Nav>
+        </Navbar.Collapse>
         {/* </Container> */}
       </Navbar>
-      <br /> 
+      <br />
     </Fragment>
   );
 };
